fcd42d09d0bd40fa25ad843e6943ef45112aef67
stilobique <aurelienvlt@free.fr>
2017-07-11 14:26:08 +0200

    Revert

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
-import ctypes, sys
-
 from BatchLightUE4.Views.GUI import UIBuildMap
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
 
 class BatchBuildSetup:
     def __init__(self):
         self.nom = 'Build Light Batch'
 
 app_name = BatchBuildSetup().nom
-app = UIBuildMap(None)
-app.title(app_name)
-print("Admin Needed")
 
-if is_admin():
-    print("Admin Ok")
+if __name__ == "__main__":
+    app = UIBuildMap(None)
+    app.title(app_name)
     app.mainloop()
-
-else:
-    # Re-run the program with admin rights
-    print("Admin pas Ok")
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", app.mainloop(), "",
-                                        None, 1)
-
-# if __name__ == "__main__":
