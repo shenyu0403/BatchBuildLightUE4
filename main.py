ec920677c531661194814004731f33d4c6cd3e57
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-08-14 00:16:04 +0200

    Create a menu and a status bar, only proof of concept and has no functional

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+
+from BatchLightUE4.Views.StatusBar import Example
 # from BatchLightUE4.Views.GUI import UIBuildMap
 
 app_info = 'Build Light Batch'
@@ -7,10 +9,12 @@ app_info = 'Build Light Batch'
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = QWidget()
-    w.resize(450, 150)
-    w.move(300, 300)
-    w.setWindowTitle(app_info)
-    w.show()
+    # w = QWidget()
+    # w.resize(450, 150)
+    # w.move(300, 300)
+    # w.setWindowTitle(app_info)
+    # w.show()
+
+    w = Example()
 
     sys.exit(app.exec_())
