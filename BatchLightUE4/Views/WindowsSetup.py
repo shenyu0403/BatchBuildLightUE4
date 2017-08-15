08e439a23f1de399c70d99cfcf09ecff4fae16d6
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-08-16 00:01:48 +0200

    Add more check control with all path and select path.

--- BatchLightUE4/Views/WindowsSetup.py
+++ BatchLightUE4/Views/WindowsSetup.py
@@ -1,5 +1,7 @@
-import sys
+import os, sys, json
 from PyQt5 import QtWidgets
+
+from ..Models.DB import paths_dict
 
 
 class Popup(QtWidgets.QWidget):
@@ -18,14 +20,26 @@ class PathPopup(Popup):
         self.WindowsPath()
 
     def WindowsPath(self):
-        self.UnrealPathLabel = QtWidgets.QLabel('Unreal Engine')
-        self.UnrealPathField = QtWidgets.QLineEdit('Choose the UE4 Editor.exe')
-        self.UnrealPathOpen = QtWidgets.QPushButton('Open')
-        self.UnrealPathOpen.clicked.connect(self.choosePath)
+        # Define base variable (path empty or not)
+        if paths_dict:
+            EditorPath = paths_dict["UE4 Editor"]
+            ProjectPath = paths_dict["UE4 Project"]
+        else:
+            EditorPath = 'UE4 Editor'
+            ProjectPath = 'uproject File'
 
+        # Setup Unreal Editor Path
+        self.UnrealPathLabel = QtWidgets.QLabel('Unreal Engine')
+        self.UnrealPathField = QtWidgets.QLineEdit(EditorPath)
+        self.UnrealPathOpen = QtWidgets.QPushButton('Open File')
+        self.UnrealPathOpen.clicked.connect(lambda :self.choosePath('Editor'))
+
+        # Setup Unreal Project Path
         self.UnrealProjectLabel = QtWidgets.QLabel('Unreal Project')
-        self.UnrealProjectField = QtWidgets.QLineEdit()
-        self.UnrealProjectOpen = QtWidgets.QPushButton('Open Uproject')
+        self.UnrealProjectField = QtWidgets.QLineEdit(ProjectPath)
+        self.UnrealProjectOpen = QtWidgets.QPushButton('Open File')
+        self.UnrealProjectOpen.clicked.connect(lambda :self.choosePath(
+            'Project'))
 
         grid = QtWidgets.QGridLayout()
         grid.setSpacing(5)
@@ -43,16 +57,36 @@ class PathPopup(Popup):
         # self.setGeometry(300, 300, 350, 300)
         self.show()
 
-    def choosePath(self):
-        path = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file',
-                                                     '/home')
+    def choosePath(self, file):
+        if paths_dict:
+            if file == 'Editor':
+                openpath = paths_dict["UE4 Editor"]
+            elif file == 'Project':
+                openpath = paths_dict["UE4 Project"]
+        else:
+            openpath = '/'
+
+        filedialog = QtWidgets.QFileDialog
+        path = filedialog.getOpenFileName(self, 'Open file', openpath)
+        textfield = ''
 
         print('Choose path : ', path[0])
+        if file == 'Editor':
+            paths_dict['UE4 Editor'] = path[0]
+            textfield = self.UnrealPathField.setText(path[0])
+            print('Save Editor')
 
-        self.UnrealPathField.setText(path[0])
+        elif file == 'Project':
+            paths_dict['UE4 Project'] = path[0]
+            print('Save Unreal project')
+            textfield = self.UnrealProjectField.setText(path[0])
 
-        return
 
+        path_json = os.path.abspath("BatchLightUE4/Models/setup.json")
+        with open(path_json, 'w') as f:
+            json.dump(paths_dict, f, indent=4)
+
+        return textfield
 
 
 if __name__ == '__main__':
