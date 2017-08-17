cfca1986d826a62736d95101c5d0883a48564819
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-08-17 23:49:33 +0200

    Split the Main Windows with many function, file menu, levels...

--- BatchLightUE4/Views/MainWindows.py
+++ BatchLightUE4/Views/MainWindows.py
@@ -1,10 +1,94 @@
-import os
+import os, json
 
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QAction, qApp, QLabel, QPushButton
 from PyQt5.QtGui import QIcon
 
 from BatchLightUE4.Views.WindowsSetup import PathPopup
+
+def show_file_bar(self):
+    '''A simple function the generate the File Menu Bar, this attribute has
+    a Qt object, the Windows.'''
+    menubar = self.menuBar()
+
+    fileMenu = menubar.addMenu('File')
+    clearAct = QAction('New', self)
+    fileMenu.addAction(clearAct)
+    exitAct = QAction(QIcon('exit.png'), 'Exit', self)
+    exitAct.setShortcut('Ctrl+Q')
+    exitAct.setStatusTip('Close this application')
+    exitAct.triggered.connect(qApp.quit)
+    fileMenu.addAction(exitAct)
+
+    fileSetup = menubar.addMenu('Setup')
+    pathAct = QAction('Configure Path', self)
+    pathAct.setStatusTip('Setup all paths needed')
+    pathAct.triggered.connect(self.menuPath)
+    fileSetup.addAction(pathAct)
+    networkAct = QAction('Network', self)
+    networkAct.setStatusTip('Setup all machine used with your network')
+    fileSetup.addAction(networkAct)
+    controlAct = QAction('Control Version', self)
+    controlAct.setStatusTip('Activate setup Control Version')
+    fileSetup.addAction(controlAct)
+
+    fileLog = menubar.addMenu('Log')
+    cleanlogAct = QAction('Clean Log', self)
+    cleanlogAct.setStatusTip('Remove your cache files')
+    fileLog.addAction(cleanlogAct)
+
+    fileHelp = menubar.addMenu('Help')
+    aboutAct = QAction('About', self)
+    aboutAct.setStatusTip('Show more information abouts this program')
+    fileHelp.addAction(aboutAct)
+    updateAct = QAction('Update', self)
+    updateAct.setStatusTip('Check the last update')
+    fileHelp.addAction(updateAct)
+    shortcutAct = QAction('Show Shortcut', self)
+    shortcutAct.setStatusTip('View and Setup all Shortcut')
+    fileHelp.addAction(shortcutAct)
+
+    return
+
+def show_levels(self):
+    '''This function show all levels if he can it.
+    She need a Windows Attribute.'''
+
+    path_json_setup = os.path.abspath("BatchLightUE4/Models/setup.json")
+    path_json_lvl = os.path.abspath("BatchLightUE4/Models/lvls_tree.json")
+    info_text = 'No level(s), setup yours paths.'
+    if os.path.isfile(path_json_setup):
+        info_text = 'All level(s)'
+
+    lvl_label = QLabel(info_text, self)
+    QtWidgets.QCheckBox('Test', self).move(120, 20)
+    lvl_label.move(20, 20)
+
+    return
+
+def builds_tree_lvls():
+    print('!! Builds Levels Tree !!')
+    path_json_setup = os.path.abspath("BatchLightUE4/Models/setup.json")
+    path_json_lvl = os.path.abspath("BatchLightUE4/Models/lvls_tree.json")
+
+    with open(path_json_setup) as f:
+        paths_dict = json.load(f)
+
+    lvls = {}
+    path_project = os.path.dirname(paths_dict['UE4 Project']) + '/Content/'
+
+    for root, dirs, files in os.walk(path_project):
+        for file in files:
+            if file.endswith('.umap'):
+                print(os.path.join(root, file))
+                lvls[file] = os.path.join(root, file)
+
+        with open(path_json_lvl, 'w') as f:
+            json.dump(lvls, f, indent=4)
+
+    debug = lvls
+
+    return debug
 
 class MainWindows(QtWidgets.QMainWindow):
     '''Main Windows, principal view, this windows can be show all level,
@@ -16,69 +100,33 @@ class MainWindows(QtWidgets.QMainWindow):
         self.initUI()
 
     def initUI(self):
-        path_json = os.path.abspath("BatchLightUE4/Models/setup.json")
-        info_text = 'No level(s), setup yours paths.'
-        if os.path.isfile(path_json):
-            info_text = 'All level(s)'
         # All Bar Menu Top
         # File | Setup | Log | About
 
-        menubar = self.menuBar()
-
-        fileMenu = menubar.addMenu('File')
-        clearAct = QAction('New', self)
-        fileMenu.addAction(clearAct)
-        exitAct = QAction(QIcon('exit.png'), 'Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Close this application')
-        exitAct.triggered.connect(qApp.quit)
-        fileMenu.addAction(exitAct)
-
-        fileSetup = menubar.addMenu('Setup')
-        pathAct = QAction('Configure Path', self)
-        pathAct.setStatusTip('Setup all paths needed')
-        pathAct.triggered.connect(self.menuPath)
-        fileSetup.addAction(pathAct)
-        networkAct = QAction('Network', self)
-        networkAct.setStatusTip('Setup all machine used with your network')
-        fileSetup.addAction(networkAct)
-        controlAct = QAction('Control Version', self)
-        controlAct.setStatusTip('Activate setup Control Version')
-        fileSetup.addAction(controlAct)
-
-        fileLog = menubar.addMenu('Log')
-        cleanlogAct = QAction('Clean Log', self)
-        cleanlogAct.setStatusTip('Remove your cache files')
-        fileLog.addAction(cleanlogAct)
-
-        fileHelp = menubar.addMenu('Help')
-        aboutAct = QAction('About', self)
-        aboutAct.setStatusTip('Show more information abouts this program')
-        fileHelp.addAction(aboutAct)
-        updateAct = QAction('Update', self)
-        updateAct.setStatusTip('Check the last update')
-        fileHelp.addAction(updateAct)
-        shortcutAct = QAction('Show Shortcut', self)
-        shortcutAct.setStatusTip('View and Setup all Shortcut')
-        fileHelp.addAction(shortcutAct)
+        show_file_bar(self)
 
         # Corps Tools
         # Listing all levels by project
         # Add a many tools, Select All, Unselected...
 
-        lvl_label = QLabel(info_text, self).move(20, 20)
-        lvl_tool = QLabel('Select Tool', self).move(20, 40)
-        lvl_build = QLabel('Build Level(s)', self).move(20, 60)
+        show_levels(self)
 
-        btn_ok = QPushButton('Builds', self).move(20, 90)
-        btn_Cancel = QPushButton('Clear', self).move(120, 90)
+
+        lvl_tool = QLabel('Select Tool', self).move(20, 60)
+        lvl_build = QLabel('Build Level(s)', self).move(20, 80)
+
+        self.btn_ok = QPushButton('Builds', self)
+        self.btn_ok.clicked.connect(self.buildProcess)
+        self.btn_ok.move(20, 110)
+        self.btn_Cancel = QPushButton('Tree Lvls', self)
+        self.btn_Cancel.clicked.connect(builds_tree_lvls)
+        self.btn_Cancel.move(120, 110)
 
         tip = QLabel('Don''t forget to update your '
                                      'repository', self)
         tip.move(20, 160)
 
         self.statusBar()
-
 
         # show the perforce user checkout
 
@@ -89,6 +137,9 @@ class MainWindows(QtWidgets.QMainWindow):
     def menuPath(self):
         self.popupPath = PathPopup()
         self.popupPath.show()
+
+    def buildProcess(self):
+        print('Launch Build')
 
 # if __name__ == '__main__':
 #     app = QtWidgets.QApplication([])
