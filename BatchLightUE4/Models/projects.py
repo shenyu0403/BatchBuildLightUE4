2210b3b705c78ab6d5c92eb0bc765f730a41f66a
stilobique <aurelienvlt@free.fr>
2017-10-05 15:51:10 +0200

    Save levels inside the SQLite

--- BatchLightUE4/Models/projects.py
+++ BatchLightUE4/Models/projects.py
@@ -17,20 +17,22 @@ class TableProgram(object):
         self.bd.cursor()
 
         if not self.bd_exist:
-            self.create_data()
+            self.create_all_tables()
 
-    def create_data(self):
+    def create_all_tables(self):
         self.bd.execute('''CREATE TABLE  projects(
                 id          INTEGER PRIMARY KEY,
-                project_id  INT,
-                paths_id    INT)''')
+                paths_id    INT,
+                levels_id   INT)''')
 
         self.bd.execute('''CREATE TABLE  paths(
                 path_id     INTEGER PRIMARY KEY,
                 editor      TEXT,
                 project     TEXT)''')
+
         self.bd.execute('''CREATE TABLE levels(
-                levels_id   INTEGER PRIMARY KEY,
+                level_id   INTEGER PRIMARY KEY,
+                project_id   INTEGER,
                 name        TEXT,
                 path        TEXT)''')
         self.bd.commit()
@@ -39,7 +41,29 @@ class TableProgram(object):
         msg_def = 'Create a news base data'
         print(msg_def)
 
+    def select_project(self):
+        self.bd.execute('''SELECT id, project_id, paths_id
+                        FROM projects''')
+        self.bd.cursor()
+
+        msg_func = 'Select a Projects'
+        print(msg_func)
+
+    def select_path(self, id_project):
+        """Select a Data path from a project used.
+        :id_project : The project working"""
+        request = self.bd.cursor()
+        request.execute('''SELECT * 
+                        FROM paths 
+                        WHERE path_id = ?''',
+                        (id_project, ))
+
+        data = request.fetchall()
+
+        return data
+
     def write_data_path(self, editor, project):
+        id_project = 0
         count_paths = self.bd.execute('''SELECT count(path_id) FROM paths''')
         count_paths = count_paths.fetchone()[0]
 
@@ -60,10 +84,39 @@ class TableProgram(object):
         print(msg_func)
 
     def write_data_levels(self):
+        id_project = 0
+        levels = {}
+        path_data = self.select_path(id_project)
+        path_data = path_data[0][2]
+        path_project = os.path.dirname(path_data) + '/Content/'
+
+        for root, dirs, files in os.walk(path_project):
+            for file in files:
+                if file.endswith('.umap'):
+                    print(os.path.join(root, file))
+                    levels[file] = os.path.join(root, file)
+
+                    cur = self.bd.cursor()
+                    cur.execute('''
+                    INSERT INTO levels(project_id, name, path) 
+                    VALUES (?, ?, ?)''',
+                                (id_project, file, root))
+
+        # INSERT INTO COMPANY VALUES(7, 'James', 24, 'Houston', 10000.00)
+        self.bd.commit()
+        self.bd.close()
+
         msg_func = 'Write a news Data Levels'
         print(msg_func)
 
-    @staticmethod
-    def read_data():
+    def debug_data(self):
+        cur = self.bd.cursor()
+        cur.execute('''SELECT * FROM paths''')
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print('Data : ', row)
+
         msg_func = 'Read Data from the base data'
         print(msg_func)
