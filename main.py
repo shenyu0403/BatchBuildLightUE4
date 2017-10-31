f02301f29b7aade4aba42385452d41e96ad21a11
stilobique <aurelienvlt@free.fr>
2017-10-31 12:11:15 +0100

    Clean unused import

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication
 
 from BatchLightUE4.Views.MainWindows import MainWindows
-# from BatchLightUE4.Views.GUI import UIBuildMap
 
 app_info = 'B-BlUE4'
 
