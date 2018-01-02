256bf329133637222f67ff09be034d993f80d416
stilobique <aurelienvlt@free.fr>
2018-01-02 18:34:20 +0100

    Name the view

--- BatchLightUE4/Views/WindowsRendering.py
+++ BatchLightUE4/Views/WindowsRendering.py
@@ -8,39 +8,39 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 96)
-        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+class Ui_Rendering(object):
+    def setupUi(self, Rendering):
+        Rendering.setObjectName("Rendering")
+        Rendering.resize(400, 96)
+        self.gridLayout = QtWidgets.QGridLayout(Rendering)
         self.gridLayout.setObjectName("gridLayout")
-        self.label_level_name = QtWidgets.QLabel(Dialog)
+        self.label_level_name = QtWidgets.QLabel(Rendering)
         self.label_level_name.setObjectName("label_level_name")
         self.gridLayout.addWidget(self.label_level_name, 0, 1, 1, 1)
-        self.progressBar = QtWidgets.QProgressBar(Dialog)
+        self.progressBar = QtWidgets.QProgressBar(Rendering)
         self.progressBar.setProperty("value", 24)
         self.progressBar.setObjectName("progressBar")
         self.gridLayout.addWidget(self.progressBar, 1, 0, 1, 2)
-        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Rendering)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Abort|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.gridLayout.addWidget(self.buttonBox, 3, 0, 1, 2)
-        self.label_level = QtWidgets.QLabel(Dialog)
+        self.label_level = QtWidgets.QLabel(Rendering)
         self.label_level.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_level.setObjectName("label_level")
         self.gridLayout.addWidget(self.label_level, 0, 0, 1, 1)
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.gridLayout.addItem(spacerItem, 2, 0, 1, 1)
 
-        self.retranslateUi(Dialog)
-        self.buttonBox.accepted.connect(Dialog.accept)
-        self.buttonBox.rejected.connect(Dialog.reject)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(Rendering)
+        self.buttonBox.accepted.connect(Rendering.accept)
+        self.buttonBox.rejected.connect(Rendering.reject)
+        QtCore.QMetaObject.connectSlotsByName(Rendering)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, Rendering):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label_level_name.setText(_translate("Dialog", "Level rendering name"))
-        self.label_level.setText(_translate("Dialog", "Level Rendering :"))
+        Rendering.setWindowTitle(_translate("Rendering", "Dialog"))
+        self.label_level_name.setText(_translate("Rendering", "Level rendering name"))
+        self.label_level.setText(_translate("Rendering", "Level Rendering :"))
 
